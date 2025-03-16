44d79c220a3dc3d01218123daf6042b64591edd8
Leonardo Diman <leonardo.diman@gmail.com>
2025-03-16 16:16:59 -0300

    chore: change file name

---
+++ src/mock/products.ts
@@ -0,0 +1,126 @@
+import TProduct from "../types/TProduct.ts";
+
+const products: TProduct[] = [
+  {
+    productName: "Produto 1",
+    productDescription: "Descrição do Produto 1",
+    productImg: "https://st2.depositphotos.com/1017228/9399/i/450/depositphotos_93990140-stock-photo-cheerful-woman-showing-blank-smartphone.jpg",
+    productPrice: 10.00
+  },
+  {
+    productName: "iPhone 12",
+    productDescription: "Apple iPhone 12 com 64GB",
+    productImg: "https://st2.depositphotos.com/1017228/9399/i/450/depositphotos_93990140-stock-photo-cheerful-woman-showing-blank-smartphone.jpg",
+    productPrice: 20.00
+  },
+  {
+    productName: "Samsung Galaxy S21",
+    productDescription: "Samsung Galaxy S21 com 128GB",
+    productImg: "https://st2.depositphotos.com/1017228/9399/i/450/depositphotos_93990140-stock-photo-cheerful-woman-showing-blank-smartphone.jpg",
+    productPrice: 30.00
+  },
+  {
+    productName: "Xiaomi Mi 11",
+    productDescription: "Xiaomi Mi 11 com 256GB",
+    productImg: "https://st2.depositphotos.com/1017228/9399/i/450/depositphotos_93990140-stock-photo-cheerful-woman-showing-blank-smartphone.jpg",
+    productPrice: 40.00
+  },
+  {
+    productName: "OnePlus 9",
+    productDescription: "OnePlus 9 com 128GB",
+    productImg: "https://st2.depositphotos.com/1017228/9399/i/450/depositphotos_93990140-stock-photo-cheerful-woman-showing-blank-smartphone.jpg",
+    productPrice: 50.00
+  },
+  {
+    productName: "Google Pixel 5",
+    productDescription: "Google Pixel 5 com 128GB",
+    productImg: "https://st2.depositphotos.com/1017228/9399/i/450/depositphotos_93990140-stock-photo-cheerful-woman-showing-blank-smartphone.jpg",
+    productPrice: 60.00
+  },
+  {
+    productName: "Sony Xperia 1 II",
+    productDescription: "Sony Xperia 1 II com 256GB",
+    productImg: "https://st2.depositphotos.com/1017228/9399/i/450/depositphotos_93990140-stock-photo-cheerful-woman-showing-blank-smartphone.jpg",
+    productPrice: 70.00
+  },
+  {
+    productName: "Huawei P40 Pro",
+    productDescription: "Huawei P40 Pro com 256GB",
+    productImg: "https://st2.depositphotos.com/1017228/9399/i/450/depositphotos_93990140-stock-photo-cheerful-woman-showing-blank-smartphone.jpg",
+    productPrice: 80.00
+  },
+  {
+    productName: "Nokia 8.3 5G",
+    productDescription: "Nokia 8.3 5G com 128GB",
+    productImg: "https://st2.depositphotos.com/1017228/9399/i/450/depositphotos_93990140-stock-photo-cheerful-woman-showing-blank-smartphone.jpg",
+    productPrice: 90.00
+  },
+  {
+    productName: "Motorola Edge Plus",
+    productDescription: "Motorola Edge Plus com 256GB",
+    productImg: "https://st2.depositphotos.com/1017228/9399/i/450/depositphotos_93990140-stock-photo-cheerful-woman-showing-blank-smartphone.jpg",
+    productPrice: 100.00
+  },
+  {
+    productName: "Asus ROG Phone 5",
+    productDescription: "Asus ROG Phone 5 com 256GB",
+    productImg: "https://st2.depositphotos.com/1017228/9399/i/450/depositphotos_93990140-stock-photo-cheerful-woman-showing-blank-smartphone.jpg",
+    productPrice: 110.00
+  },
+  {
+    productName: "LG Wing",
+    productDescription: "LG Wing com 128GB",
+    productImg: "https://st2.depositphotos.com/1017228/9399/i/450/depositphotos_93990140-stock-photo-cheerful-woman-showing-blank-smartphone.jpg",
+    productPrice: 120.00
+  },
+  {
+    productName: "Oppo Find X3 Pro",
+    productDescription: "Oppo Find X3 Pro com 256GB",
+    productImg: "https://st2.depositphotos.com/1017228/9399/i/450/depositphotos_93990140-stock-photo-cheerful-woman-showing-blank-smartphone.jpg",
+    productPrice: 130.00
+  },
+  {
+    productName: "Realme GT",
+    productDescription: "Realme GT com 128GB",
+    productImg: "https://st2.depositphotos.com/1017228/9399/i/450/depositphotos_93990140-stock-photo-cheerful-woman-showing-blank-smartphone.jpg",
+    productPrice: 140.00
+  },
+  {
+    productName: "Vivo X60 Pro",
+    productDescription: "Vivo X60 Pro com 256GB",
+    productImg: "https://st2.depositphotos.com/1017228/9399/i/450/depositphotos_93990140-stock-photo-cheerful-woman-showing-blank-smartphone.jpg",
+    productPrice: 150.00
+  },
+  {
+    productName: "ZTE Axon 30 Ultra",
+    productDescription: "ZTE Axon 30 Ultra com 256GB",
+    productImg: "https://st2.depositphotos.com/1017228/9399/i/450/depositphotos_93990140-stock-photo-cheerful-woman-showing-blank-smartphone.jpg",
+    productPrice: 160.00
+  },
+  {
+    productName: "Lenovo Legion Phone Duel",
+    productDescription: "Lenovo Legion Phone Duel com 256GB",
+    productImg: "https://st2.depositphotos.com/1017228/9399/i/450/depositphotos_93990140-stock-photo-cheerful-woman-showing-blank-smartphone.jpg",
+    productPrice: 170.00
+  },
+  {
+    productName: "iPhone 12 Pro Max",
+    productDescription: "Apple iPhone 12 Pro Max com 512GB",
+    productImg: "https://st2.depositphotos.com/1017228/9399/i/450/depositphotos_93990140-stock-photo-cheerful-woman-showing-blank-smartphone.jpg",
+    productPrice: 180.00
+  },
+  {
+    productName: "Samsung Galaxy Note 20 Ultra",
+    productDescription: "Samsung Galaxy Note 20 Ultra com 512GB",
+    productImg: "https://st2.depositphotos.com/1017228/9399/i/450/depositphotos_93990140-stock-photo-cheerful-woman-showing-blank-smartphone.jpg",
+    productPrice: 190.00
+  },
+  {
+    productName: "Xiaomi Mi 10T Pro",
+    productDescription: "Xiaomi Mi 10T Pro com 256GB",
+    productImg: "https://st2.depositphotos.com/1017228/9399/i/450/depositphotos_93990140-stock-photo-cheerful-woman-showing-blank-smartphone.jpg",
+    productPrice: 200.00
+  }
+];
+
+export default products;
