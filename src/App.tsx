04fece4c6e21b86c6ce8627a9253e06e2c13e451
Leonardo Diman <leonardo.diman@gmail.com>
2025-03-15 23:27:04 -0300

    chore: configs update + logo + mock

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import MainSection from "./components/MainSection";
+import FilterSection from "./components/FilterSection";
+
 export default function App () {
   return (
-    <main className='bg-primary-main text-secondary-main text-center text-6xl py-5'>
-      oi
-    </main>
+    <div className='w-full min-h-screen h-full bg-zinc-200'>
+      <Header />
+      <main className='w-full flex flex-row max-w-screen-xl mx-auto p-3 lg:px-0 lg:pt-4 pb-12 lg:pb-14'>
+        <FilterSection />
+        <MainSection />
+      </main>
+      <Footer />
+    </div>
   )
 }
