957608f50abb49d49814acafda619963a674a032
Leonardo Diman <leonardo.diman@gmail.com>
2025-03-17 21:09:10 -0300

    feat: tailwind var + remove router

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import {StrictMode} from 'react'
+import {createRoot} from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import AppProvider from "./context/AppProvider.tsx";
-import {BrowserRouter as Router} from "react-router-dom";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Router>
-      <AppProvider>
-        <App />
-      </AppProvider>
-    </Router>
+    <AppProvider>
+      <App/>
+    </AppProvider>
   </StrictMode>
 )
