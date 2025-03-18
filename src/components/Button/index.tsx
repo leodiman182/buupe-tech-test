9c680b4bf385b2eadc9e5ddea8490f301bd92712
Leonardo Diman <leonardo.diman@gmail.com>
2025-03-17 21:32:05 -0300

    fix: button style via tailwind

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,10 +1,16 @@
 import {ButtonHTMLAttributes, ReactNode} from "react";
-import './style.css'
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   children: ReactNode;
 }
 
 export default function Button({ children, ...props }: ButtonProps) {
-  return <button {...props} className='p-1 text-white duration-125 cursor-pointer rounded-md w-1/2 lg:max-w-24 button'>{children}</button>;
+  return (
+    <button
+      className='bg-primary hover:bg-secondary p-1 text-white duration-125 cursor-pointer rounded-md w-1/2 lg:max-w-24 button'
+      { ...props }
+    >
+      { children }
+    </button>
+  );
 }
